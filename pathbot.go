59951b1ec3ccab683a3e7def1cb5a56fe4aea667
vicky-sunshine <lch82327@gmail.com>
2019-07-12 02:12:01 +0800

    use DFS to solve pathbot

--- pathbot.go
+++ pathbot.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
 )
 
@@ -26,63 +24,98 @@ type PathbotLocation struct {
 	LocationPath      string   `json:"locationPath"`
 }
 
-func main() {
-	var location = start()
+type RoomID struct {
+	X int
+	Y int
+}
 
-	explore(location)
+func (r RoomID) String() string {
+	return fmt.Sprintf("(%v, %v)", r.X, r.Y)
+}
+
+// global visit map
+var visit = make(map[RoomID]bool)
+
+func main() {
+	startLocation := start()
+	startRoomID := RoomID{X: 0, Y: 0}
+
+	DFSWalk(startLocation, startRoomID)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func DFSWalk(location PathbotLocation, roomID RoomID) (finished bool) {
+	// print and save location info
+	printLocation(location, roomID)
+	visit[roomID] = true
+
+	// check if game is finished
+	if location.Status == "finished" {
+		return true
+	}
+
+	// decide next direction
+	printPrompt(location.Exits)
+	for _, v := range location.Exits {
+		var nextRoomID RoomID
+		switch v {
+		case "N":
+			nextRoomID = RoomID{X: roomID.X + 1, Y: roomID.Y}
+		case "S":
+			nextRoomID = RoomID{X: roomID.X - 1, Y: roomID.Y}
+		case "E":
+			nextRoomID = RoomID{X: roomID.X, Y: roomID.Y + 1}
+		case "W":
+			nextRoomID = RoomID{X: roomID.X, Y: roomID.Y - 1}
+		default:
+			panic(fmt.Errorf("Un-possible direction %v", v))
+		}
+
+		if visited, _ := visit[nextRoomID]; !visited {
+			printDecision(v)
+			dir := PathbotDirection{Direction: v}
+			body, err := json.Marshal(dir)
+			checkError(err)
+
+			nextLocation := apiPost(location.LocationPath, bytes.NewBuffer(body))
+			if finished := DFSWalk(nextLocation, nextRoomID); finished {
+				return finished
+			}
+		}
+	}
+
+	return false
+}
+
+// pathbot interact interface
+func printPrompt(directions []string) {
+	fmt.Println("What direction will you go?", directions)
+}
+
+func printDecision(direction string) {
+	fmt.Println("Choose", direction)
+}
+
+func printLocation(location PathbotLocation, roomID RoomID) {
+	fmt.Println()
+	fmt.Println("[", location.Status, "|", "Room ID:", roomID, "]")
+	fmt.Println(location.Message)
+	fmt.Println(location.Description)
 }
 
 func start() PathbotLocation {
 	return apiPost("/pathbot/start", strings.NewReader("{}"))
 }
 
-func explore(location PathbotLocation) {
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		printLocation(location)
-
-		if location.Status == "finished" {
-			fmt.Println(location.Message)
-			os.Exit(0)
-		}
-
-		printPrompt(location.Exits)
-
-		direction, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err.Error())
-		}
-
-		dir := PathbotDirection{Direction: strings.ToUpper(direction[0:1])}
-
-		body, err := json.Marshal(dir)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		location = apiPost(location.LocationPath, bytes.NewBuffer(body))
-	}
-}
-
-func printPrompt(directions []string) {
-	fmt.Println("What direction will you go?")
-	fmt.Println(directions)
-}
-
-func printLocation(location PathbotLocation) {
-	fmt.Println()
-	fmt.Println(location.Message)
-	fmt.Println(location.Description)
-}
-
 func apiPost(path string, body io.Reader) PathbotLocation {
 	domain := "https://api.noopschallenge.com"
 	res, err := http.Post(domain+path, "application/json", body)
-	if err != nil {
-		panic(err.Error())
-	}
+	checkError(err)
 
 	return parseResponse(res)
 }
@@ -90,14 +123,10 @@ func apiPost(path string, body io.Reader) PathbotLocation {
 func parseResponse(res *http.Response) PathbotLocation {
 	var response PathbotLocation
 	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err.Error())
-	}
+	checkError(err)
 
 	err = json.Unmarshal(body, &response)
-	if err != nil {
-		panic(err.Error())
-	}
+	checkError(err)
 
 	return response
 }
